13ccc85318b62e9b7cea24301423a3d764ef7cc5
venyii <schefflor@gmail.com>
2012-12-27 01:09:59 +0100

    [ADD] compiler

--- src/Statika/Compiler.php
+++ src/Statika/Compiler.php
@@ -1,6 +1,16 @@
 <?php
 
 /*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
  * This file is part of Statika.
  *
  * (c) Sven Scheffler <schefflor@gmail.com>
@@ -11,14 +21,154 @@
 
 namespace Statika;
 
+use Symfony\Component\Finder\Finder;
+
 /**
  * @author Sven Scheffler <schefflor@gmail.com>
- * @todo implement
  */
 class Compiler
 {
-    public function compile()
+    /**
+     * Compile Statika
+     *
+     * @param string $pharFile
+     */
+    public function compile($pharFile = 'statika.phar')
     {
+        if (file_exists($pharFile)) {
+            unlink($pharFile);
+        }
+
+        $phar = new \Phar($pharFile, 0, 'statika.phar');
+        $phar->setSignatureAlgorithm(\Phar::SHA1);
+
+        $phar->startBuffering();
+
+        $finder = new Finder();
+        $finder->files()
+                ->ignoreVCS(true)
+                ->name('*.php')
+                ->notName('Compiler.php')
+                ->notName('ClassLoader.php')
+                ->in(__DIR__ . '/..')
+        ;
+
+        foreach ($finder as $file) {
+            $this->addFile($phar, $file);
+        }
+
+        $finder = new Finder();
+        $finder->files()
+                ->ignoreVCS(true)
+                ->name('*.php')
+                ->exclude('Tests')
+                ->in(__DIR__ . '/../../vendor/symfony/')
+        ;
+
+        foreach ($finder as $file) {
+            $this->addFile($phar, $file);
+        }
+
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/autoload.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_namespaces.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_classmap.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_real.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/ClassLoader.php'));
+        $this->addStatikaBin($phar);
+
+        // Stubs
+        $phar->setStub($this->getStub());
+
+        $phar->stopBuffering();
+
+        // disabled for interoperability with systems without gzip ext
+        $phar->compressFiles(\Phar::GZ);
+
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../LICENSE'), false);
+
+        unset($phar);
+    }
+
+    private function addFile($phar, $file, $strip = true)
+    {
+        $path = str_replace(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR, '', $file->getRealPath());
+
+        $content = file_get_contents($file);
+        if ($strip) {
+            $content = $this->stripWhitespace($content);
+        } elseif ('LICENSE' === basename($file)) {
+            $content = "\n" . $content . "\n";
+        }
+
+//        $content = str_replace('@package_version@', $this->version, $content);
+
+        $phar->addFromString($path, $content);
+    }
+
+    private function addStatikaBin($phar)
+    {
+        $content = file_get_contents(__DIR__ . '/../../bin/statika');
+        $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
+        $phar->addFromString('bin/statika', $content);
+    }
+
+    /**
+     * Removes whitespace from a PHP source string while preserving line numbers.
+     *
+     * @param  string $source A PHP string
+     * @return string The PHP string with the whitespace removed
+     */
+    private function stripWhitespace($source)
+    {
+        if (!function_exists('token_get_all')) {
+            return $source;
+        }
+
+        $output = '';
+        foreach (token_get_all($source) as $token) {
+            if (is_string($token)) {
+                $output .= $token;
+            } elseif (in_array($token[0], array(T_COMMENT, T_DOC_COMMENT))) {
+                $output .= str_repeat("\n", substr_count($token[1], "\n"));
+            } elseif (T_WHITESPACE === $token[0]) {
+                // reduce wide spaces
+                $whitespace = preg_replace('{[ \t]+}', ' ', $token[1]);
+                // normalize newlines to \n
+                $whitespace = preg_replace('{(?:\r\n|\r|\n)}', "\n", $whitespace);
+                // trim leading spaces
+                $whitespace = preg_replace('{\n +}', "\n", $whitespace);
+                $output .= $whitespace;
+            } else {
+                $output .= $token[1];
+            }
+        }
+
+        return $output;
+    }
+
+    private function getStub()
+    {
+        $stub = <<<'EOF'
+#!/usr/bin/env php
+<?php
+/*
+ * This file is part of Statika.
+ *
+ * (c) Sven Scheffler <schefflor@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+Phar::mapPhar('statika.phar');
+
+EOF;
+
+        return $stub . <<<'EOF'
+require 'phar://statika.phar/bin/statika';
+
+__HALT_COMPILER();
+EOF;
     }
 
 }
