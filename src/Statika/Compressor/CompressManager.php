9a04baa0098b00b0acf7a7b63f01a9e27f6a7707
Sven Scheffler <schefflor@gmail.com>
2013-01-23 00:14:24 +0100

    [FIX] namespace case

--- src/Statika/Compressor/CompressManager.php
+++ src/Statika/Compressor/CompressManager.php
@@ -14,7 +14,7 @@ namespace Statika\Compressor;
 use Statika\Version\Version;
 use Statika\File\FileAggregator;
 use Statika\Compressor\Compressor;
-use Statika\Configuration\COmposition\CompositionConfiguration;
+use Statika\Configuration\Composition\CompositionConfiguration;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 
